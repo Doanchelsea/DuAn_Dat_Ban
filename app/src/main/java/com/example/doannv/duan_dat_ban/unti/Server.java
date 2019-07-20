c692bc5df8a9b2362386209bcefcc68cce36a3fd
DoanNV <doannvph05829@fpt.edu.vn>
2019-07-21 00:34:31 +0700

    Initial commit

---
+++ app/src/main/java/com/example/doannv/duan_dat_ban/unti/Server.java
@@ -0,0 +1,17 @@
+package com.example.doannv.duan_dat_ban.unti;
+
+public class Server {
+//    public static String localhost = "10.255.157.106";
+    public static String localhost = "192.168.1.8";
+//    public static String localhost = "10.22.205.154";
+    public static String duongdandangnhap = "http://"+localhost+"/datban/dangnhap.php";
+    public static String duongdananh = "http://"+localhost+"/datban/";
+    public static String duongdanlayout1 = "http://"+localhost+"/datban/bangtin.php";
+    public static String duongdansangtrong = "http://"+localhost+"/datban/sangtrong.php";
+    public static String duongdanbinhdan= "http://"+localhost+"/datban/binhdan.php";
+    public static String duongdangiare= "http://"+localhost+"/datban/giare.php";
+    public static String duongdanbinhdanchitiet= "http://"+localhost+"/datban/binhdanchitiet.php";
+    public static String duongdanyeuthich= "http://"+localhost+"/datban/yeuthich.php";
+    public static String duongdanyeuthichthem= "http://"+localhost+"/datban/yeuthichthem.php";
+    public static String duongdanyeuthichxoa= "http://"+localhost+"/datban/xoayeuthich.php";
+}
